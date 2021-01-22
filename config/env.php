d3f72ad1e2e7712d6f71d2617abd92364cb306f3
Nathan <GUILLE16Na@lycee-ndduroc.com>
2021-01-22 17:46:26 +0100

    INITIALISATION PROJET
    Signed-off-by: Nathan <GUILLE16Na@lycee-ndduroc.com>

--- config/env.php
+++ config/env.php
@@ -5,9 +5,9 @@ const ENV =[
     'APP_NAME'    => 'Lypso',
     'APP_VERSION' => 'v0',
     // Paramètres pour l'accès à la base de données
-    'DB_HOST'     => 'localhost',
-    'DB_DATABASE' => 'lypso',
-    'DB_USERNAME' => 'lypso',
-    'DB_PASSWORD' => 'lypso'
+    'DB_HOST'     => '127.0.0.1',
+    'DB_DATABASE' => 'lypso_v0',
+    'DB_USERNAME' => 'usersio',
+    'DB_PASSWORD' => 'pwsio'
 ];
 
