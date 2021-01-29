d560ca3148a01670d9a3381c3c93243cf9e77345
Nathan <GUILLE16Na@lycee-ndduroc.com>
2021-01-29 15:24:19 +0100

    MAJ: Management
    Signed-off-by: Nathan <GUILLE16Na@lycee-ndduroc.com>

--- data/managementData.php
+++ data/managementData.php
@@ -3,6 +3,7 @@
 
 
 function managementRequests_getAll(){
-    $request="SELECT * FROM vacation WHERE status=0";
+    //$request="SELECT * FROM vacation WHERE status=0";
+    $request="SELECT * FROM vacation JOIN user ON vacation.user_id=user.id JOIN department ON user.department_id=department.id WHERE vacation.status=0";
     return Connection::query($request);
 }
--- page/managementPage_default.php
+++ page/managementPage_default.php
@@ -9,10 +9,11 @@
 
 
 
-        <table class="table table-striped table-bordered">
+        <table class="table table-bordered">
             <thead>
             <tr>
                 <th>NOM : Prénom</th>
+                <th>Departement</th>
                 <th>Début</th>
                 <th>Fin</th>
                 <th>Status</th>
@@ -26,11 +27,19 @@
             foreach ($actualRequests as $actualRequest){
                 ?>
                 <tr>
-                    <td></td>
-                    <td><?php echo $actualRequest['start'] ?></td>
-                    <td><?php echo $actualRequest['end'] ?></td>
+                    <td><?php echo strtoupper($actualRequest['lastname']) . " " . ucfirst(strtolower($actualRequest['firstname']))?></td>
+
+                    <td><?php echo $actualRequest['nom'] ?></td>
+                    <td><?php echo date('d/m/y', strtotime($actualRequest['start']))?></td>
+                    <td><?php echo date('d/m/y', strtotime($actualRequest['end'])) ?></td>
                     <td><?php echo $actualRequest['status']?></td>
-                    <td></td>
+                    <td><a class="btn bg-olive btn-xs">Accepter</a>
+                        &nbsp;
+                        <a class="btn bg-navy btn-xs">Annuler</a>
+                        &nbsp;
+                        <a class="btn bg-danger btn-xs">Annuler</a>
+
+                    </td>
                 </tr>
                 <?php
             }
