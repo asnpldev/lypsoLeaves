090512b67d181014c6f8ca264b6b2c3f1dcbd245
ThomasCha <changthomas85@gmail.com>
2021-01-29 17:10:52 +0100

    17h00 création demande de congé

--- data/vacationData.php
+++ data/vacationData.php
@@ -1,6 +1,7 @@
 <?php
 
 function vacation_storeAction($datas){
-    $request='INSERT INTO vacation VALUES(NULL, $datas[start], $datas[end], $datas[user_id], datas[status])';
-    return Connection::exec($request);
+    $request="INSERT INTO vacation VALUES(NULL, ".$datas['start'].', '.$datas['end'].', '".$datas['user_id']."', 0)";
+    $results=Connection::exec($request);
+    return $results;
 }
