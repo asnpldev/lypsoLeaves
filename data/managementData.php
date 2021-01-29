8d7b5d098087d8c9e93fecd127ed29fb878e9135
Nathan <GUILLE16Na@lycee-ndduroc.com>
2021-01-29 16:41:09 +0100

    MAJ: Management
    Signed-off-by: Nathan <GUILLE16Na@lycee-ndduroc.com>

--- data/managementData.php
+++ data/managementData.php
@@ -3,7 +3,6 @@
 
 
 function managementRequests_getAll(){
-    //$request="SELECT * FROM vacation WHERE status=0";
-    $request="SELECT * FROM vacation JOIN user ON vacation.user_id=user.id JOIN department ON user.department_id=department.id WHERE vacation.status=0";
+    $request="SELECT * FROM vacation JOIN user ON vacation.user_id=user.id JOIN department ON user.department_id=department.id WHERE vacation.status=0 ORDER BY vacation.start ASC";
     return Connection::query($request);
 }
--- page/managementPage_default.php
+++ page/managementPage_default.php
@@ -1,50 +1,99 @@
-
-<?php include ('../page/template/header.php'); ?>
+<?php include('../page/template/header.php'); ?>
 
     <div class="card-header">
         Gestion des congés
     </div>
     <div class="card-body">
+        <div class="row">
+            <div class="col-lg-6">
+
+                <!--        <table class="table table-bordered">-->
+                <!--            <thead>-->
+                <!--            <tr>-->
+                <!--                <th>NOM : Prénom</th>-->
+                <!--                <th>Departement</th>-->
+                <!--                <th>Début</th>-->
+                <!--                <th>Fin</th>-->
+                <!--                <th>Actions</th>-->
+                <!--            </tr>-->
+                <!--            </thead>-->
+                <!--            <tbody>-->
+                <!---->
+                <!--            --><?php
+                //
+                //            foreach ($actualRequests as $actualRequest){
+                //                ?>
+                <!--                <tr>-->
+                <!--                    <td></td>-->
+                <!---->
+                <!--                    <td></td>-->
+                <!--                    <td></td>-->
+                <!--                    <td></td>-->
+                <!---->
+                <!--                    <td><a class="btn bg-olive btn-xs">Accepter</a>-->
+                <!--                        &nbsp;-->
+                <!--                        <a class="btn bg-navy btn-xs">Annuler</a>-->
+                <!--                        &nbsp;-->
+                <!--                        <a class="btn bg-danger btn-xs">Annuler</a>-->
+                <!---->
+                <!--                    </td>-->
+                <!--                </tr>-->
+                <!--                --><?php
+                //            }
+                //            ?>
+                <!--            </tbody>-->
+                <!--        </table>-->
 
 
+                <br>
+                <?php foreach ($actualRequests as $actualRequest) {
+                    ?>
+                    <div class="post">
+                        <div class="row">
+                            <div class="col-md-5">
+                                <div class="user-block">
+                                    <img class="img-circle img-bordered-sm"
+                                         src="https://cdn4.iconfinder.com/data/icons/small-n-flat/24/user-alt-512.png"
+                                         alt="user image">
+                                    <span class="username">
+                          <?php echo strtoupper($actualRequest['lastname']) . " " . ucfirst(strtolower($actualRequest['firstname'])) ?>
+
+                        </span>
+                                    <span class="description">À fait une demande de congés</span>
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+
+                                <div class="user-block">
 
 
-        <table class="table table-bordered">
-            <thead>
-            <tr>
-                <th>NOM : Prénom</th>
-                <th>Departement</th>
-                <th>Début</th>
-                <th>Fin</th>
-                <th>Actions</th>
-            </tr>
-            </thead>
-            <tbody>
+                                    <i class="fas fa-calendar-week"></i>
 
-            <?php
+                                    <code><?php echo date('d/m/y', strtotime($actualRequest['start'])) ?></code>
 
-            foreach ($actualRequests as $actualRequest){
+                                    ➜ <code><?php echo date('d/m/y', strtotime($actualRequest['end'])) ?></code>
+
+                                    <br><span class="description"
+                                              style="margin-left: 0px;">Département: <code><?php echo $actualRequest['nom'] ?></code></span>
+
+
+                                </div>
+                            </div>
+                            <div class="col-md-3">
+                        <span style="float: right">
+                        <a href="#" class="badge bg-olive">Valider</a>&nbsp;
+                        <a href="#" class="badge bg-navy">Supprimer</a>
+                            </span>
+                            </div>
+                        </div>
+                    </div>
+
+                    <?php
+                }
                 ?>
-                <tr>
-                    <td><?php echo strtoupper($actualRequest['lastname']) . " " . ucfirst(strtolower($actualRequest['firstname']))?></td>
+            </div>
+        </div>
 
-                    <td><?php echo $actualRequest['nom'] ?></td>
-                    <td><?php echo date('d/m/y', strtotime($actualRequest['start']))?></td>
-                    <td><?php echo date('d/m/y', strtotime($actualRequest['end'])) ?></td>
-
-                    <td><a class="btn bg-olive btn-xs">Accepter</a>
-                        &nbsp;
-                        <a class="btn bg-navy btn-xs">Annuler</a>
-                        &nbsp;
-                        <a class="btn bg-danger btn-xs">Annuler</a>
-
-                    </td>
-                </tr>
-                <?php
-            }
-            ?>
-            </tbody>
-        </table>
     </div>
 
-<?php include ('../page/template/footer.php'); ?>
+<?php include('../page/template/footer.php'); ?>
