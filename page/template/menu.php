f261a8df559b132db20dc3af8c464dbeba31f77b
Nathan <GUILLE16Na@lycee-ndduroc.com>
2021-02-10 12:00:53 +0100

    MAJ: New module
    Signed-off-by: Nathan <GUILLE16Na@lycee-ndduroc.com>

--- page/template/menu.php
+++ page/template/menu.php
@@ -1,7 +1,7 @@
 <?php
 
 $temp = $_GET['route'];
- ?>
+?>
 
 <div class="grid grid-cols-12">
     <div class="col-span-3">
@@ -12,39 +12,42 @@ $temp = $_GET['route'];
             <div class="col-span-6 bg-gray-800 h-screen p-3">
 
 
+                <div class="w-full text-left m-4 transition duration-200 ease-in-out transform hover:translate-x-2">
+                    <a href="?route=dashboard"
+                       class="<?php if ($temp == "dashboard") { ?>text-indigo-300<?php } else { ?> text-gray-300 <?php } ?> px-3 py-2 rounded-md text-sm font-medium hover:bg-gray-900 "><i
+                                class="fas fa-columns mr-4"></i> Tableau
+                        de bord</a>
+                </div>
 
+                <div class="w-full text-left m-4 transition duration-200 ease-in-out transform hover:translate-x-2">
+                    <a href="?route=management"
+                       class="<?php if ($temp == "management") { ?>text-indigo-300<?php } else { ?> text-gray-300 <?php } ?> px-3 py-2 rounded-md text-sm font-medium hover:bg-gray-900"><i
+                                class="fas fa-tasks mr-4"></i>Management</a>
+                </div>
 
+                <div class="w-full text-left m-4 transition duration-200 ease-in-out transform hover:translate-x-2">
+                    <a href="?route=vacation"
+                       class="<?php if ($temp == "vacation") { ?>text-indigo-300<?php } else { ?> text-gray-300 <?php } ?> px-3 py-2 rounded-md text-sm font-medium hover:bg-gray-900"><i
+                                class="fas fa-map-marker mr-4"></i>Demande de congés</a>
+                </div>
 
-
-
-                        <div class="w-full text-left m-4 transition duration-200 ease-in-out transform hover:translate-x-2">
-                            <a href="?route=dashboard" class="<?php if($temp == "dashboard"){?>text-indigo-300<?php }else {?> text-gray-300 <?php } ?> px-3 py-2 rounded-md text-sm font-medium hover:bg-gray-900 "><i class="fas fa-columns mr-4"></i> Tableau
-                                de bord</a>
-                        </div>
-
-                        <div class="w-full text-left m-4 transition duration-200 ease-in-out transform hover:translate-x-2">
-                            <a href="?route=management" class="<?php if($temp == "management"){?>text-indigo-300<?php }else {?> text-gray-300 <?php } ?> px-3 py-2 rounded-md text-sm font-medium hover:bg-gray-900"><i class="fas fa-tasks mr-4"></i>Management</a>
-                        </div>
-
-                        <div class="w-full text-left m-4 transition duration-200 ease-in-out transform hover:translate-x-2">
-                            <a href="?route=vacation" class="<?php if($temp == "vacation"){?>text-indigo-300<?php }else {?> text-gray-300 <?php } ?> px-3 py-2 rounded-md text-sm font-medium hover:bg-gray-900"><i class="fas fa-map-marker mr-4"></i>Demande de congés</a>
-                        </div>
-
-                        <div class="w-full text-left m-4 transition duration-200 ease-in-out transform hover:translate-x-2">
-                            <a href="?route=agenda" class="<?php if($temp == "agenda"){?>text-indigo-300<?php }else {?> text-gray-300 <?php } ?> px-3 py-2 rounded-md text-sm font-medium hover:bg-gray-900"><i class="fas fa-calendar-week mr-4"></i>Agenda</a>
-                        </div>
-
-
-
+                <div class="w-full text-left m-4 transition duration-200 ease-in-out transform hover:translate-x-2">
+                    <a href="?route=agenda"
+                       class="<?php if ($temp == "agenda") { ?>text-indigo-300<?php } else { ?> text-gray-300 <?php } ?> px-3 py-2 rounded-md text-sm font-medium hover:bg-gray-900"><i
+                                class="fas fa-calendar-week mr-4"></i>Agenda</a>
+                </div>
 
 
             </div>
             <div class="col-span-6 bg-gray-200">
 
-<?php
+                <?php
                 switch ($temp) {
                     case 'management':
                         include('modules/managementModule.php');
+                        break;
+                    case 'dashboard':
+                        include('modules/dashboardModule.php');
                         break;
 
                     default:
@@ -52,9 +55,6 @@ $temp = $_GET['route'];
                         break;
 
                 }
-
-
-
 
 
                 ?>
---
+++ page/template/modules/dashboardModule.php
@@ -0,0 +1,16 @@
+<div class="bg-gray-200">
+    <div class="px-4 py-5 sm:px-6">
+        <h3 class="text-lg leading-6 font-medium text-gray-900">
+            Modules
+        </h3>
+        <p class="mt-1 max-w-2xl text-sm font-regular text-gray-500">
+
+
+            <a href="#" class="text-red-600 bg-red-200 px-2 inline-flex text-xs leading-5 rounded-full">Aucun module disponible</a>
+
+        </p>
+    </div>
+</div>
+
+
+
