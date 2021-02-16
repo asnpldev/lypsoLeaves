54c1eb42991feb6526d719645c62176646602875
asnpldev <GUILLE16Na@lycee-ndduroc.com>
2021-02-16 12:10:58 +0100

    ADD: PERMS

--- page/template/modules/adminModule.php
+++ page/template/modules/adminModule.php
@@ -33,7 +33,7 @@
             </a>
             <a href="?route=admin&action=permissions" class="w-full text-sm text-gray-800 py-2 inline-block text-left">
                 <span class="text-xl p-2.5 font-extrabold">#</span>
-                PERMISSIONS
+                PERMISSIONS (user)
             </a>
 
 
