53dfb76536271510845fdd00862a7b0aada2c784
Nathan <GUILLE16Na@lycee-ndduroc.com>
2021-02-17 16:36:57 +0100

    MAJ: Management agenda
    Signed-off-by: Nathan <GUILLE16Na@lycee-ndduroc.com>

--- page/managementPage_overview.php
+++ page/managementPage_overview.php
@@ -132,17 +132,33 @@
                                 <div class="flex flex-row items-center justify-between">
                                     <div class="flex flex-col">
                                         <div class="text-xs uppercase font-light text-gray-500">
-
+                                            Demande validée par
                                         </div>
                                         <div class="text-xl font-bold">
-
+                                            <a href="#" class="text-indigo-600 ">usersio</a>
                                         </div>
                                     </div>
-                                    <i class="far fa-adjust text-xl text-gray-500"></i>
+                                    <i class="far fa-check-circle text-xl text-gray-500"></i>
                                 </div>
                             </div>
                         </div>
                     </div>
+
+
+                <br>
+
+                <div class="h-64 rounded-md overflow-hidden bg-cover bg-center" style="background-image: url('../page/img/snowbg.jpg')">
+                    <div class="bg-gray-900 bg-opacity-50 flex items-center h-full">
+                        <div class="px-10 max-w-xl">
+                            <h2 class="text-2xl text-white font-semibold">Sport Shoes</h2>
+                            <p class="mt-2 text-gray-400">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore facere provident molestias ipsam sint voluptatum pariatur.</p>
+                            <button class="flex items-center mt-4 px-3 py-2 bg-blue-600 text-white text-sm uppercase font-medium rounded hover:bg-blue-500 focus:outline-none focus:bg-blue-500">
+                                <span>Shop Now</span>
+                                <svg class="h-5 w-5 mx-2" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M17 8l4 4m0 0l-4 4m4-4H3"></path></svg>
+                            </button>
+                        </div>
+                    </div>
+                </div>
 
 
 
